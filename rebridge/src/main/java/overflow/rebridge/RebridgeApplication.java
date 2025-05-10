a38929351d0e71fe4886c86780c622293362a6b8
Youngin <hardworkingini@gmail.com>
2025-05-10 19:01:03 +0900

    feat : application-secret도 추가

--- rebridge/src/main/java/overflow/rebridge/RebridgeApplication.java
+++ rebridge/src/main/java/overflow/rebridge/RebridgeApplication.java
@@ -2,6 +2,7 @@ package overflow.rebridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,7 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class RebridgeApplication {
 
 	public static void main(String[] args) {
+		new SpringApplicationBuilder(RebridgeApplication.class)
+				.properties("spring.config.location=classpath:/application.yml,classpath:/application-secret.yml")
+				.run(args);
 		SpringApplication.run(RebridgeApplication.class, args);
 	}
-
 }
