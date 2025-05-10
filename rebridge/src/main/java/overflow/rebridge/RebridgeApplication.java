a5ba51c05358805a79d2efe1cca70ac250c44a94
Youngin <hardworkingini@gmail.com>
2025-05-10 19:04:16 +0900

    feat : Springboot가 두번 실행되지 않도록 수정

--- rebridge/src/main/java/overflow/rebridge/RebridgeApplication.java
+++ rebridge/src/main/java/overflow/rebridge/RebridgeApplication.java
@@ -5,14 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaAuditing
 public class RebridgeApplication {
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(RebridgeApplication.class)
-				.properties("spring.config.location=classpath:/application.yml,classpath:/application-secret.yml")
-				.run(args);
 		SpringApplication.run(RebridgeApplication.class, args);
 	}
 }
+
