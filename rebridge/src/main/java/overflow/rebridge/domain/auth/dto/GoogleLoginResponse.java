909e74da8ae1eb2669ebb8d28256323e2ee437ba
Youngin <hardworkingini@gmail.com>
2025-05-12 02:28:46 +0900

    feat : request, response 정리

---
+++ rebridge/src/main/java/overflow/rebridge/domain/auth/dto/GoogleLoginResponse.java
@@ -0,0 +1,4 @@
+package overflow.rebridge.domain.auth.dto;
+
+public record GoogleLoginResponse(String name, String email, String role, String loginType, String accessToken, String refreshToken) {
+}
